80c43a524d10a6cf9d3c8a0e353fb23cd6b28b61
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2024-10-11 11:28:56 +0200

    Project setup

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Counter from './components/Counter.vue';
+</script>
+
+<template>
+    <h1 class="text-center">Hello World!</h1>
+    <Counter />
+</template>
+
+<style scoped>
+/* Add any styles if needed */
+</style>
