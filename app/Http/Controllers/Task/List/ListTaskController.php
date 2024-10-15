2db3a98a50bc6a409c3cf1f79c0e058c6c202dbe
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2024-10-15 12:08:31 +0200

    Simplifies Task listing

--- app/Http/Controllers/Task/List/ListTaskController.php
+++ app/Http/Controllers/Task/List/ListTaskController.php
@@ -5,16 +5,14 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Task\List;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task\Task;
 use Illuminate\Http\JsonResponse;
 
 class ListTaskController extends Controller
 {
     public function list(): JsonResponse
     {
-        $query = new ListTaskQuery();
-
-        $tasks = ListTaskQueryHandler::handle($query);
-
+        $tasks = Task::whereIn('status', ['scheduled', 'in_progress'])->get();
         return response()->json($tasks);
     }
 }
