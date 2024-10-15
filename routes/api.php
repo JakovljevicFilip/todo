4f9d46e5113e5aab972eb77a90a52f08667edf3e
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2024-10-15 12:06:31 +0200

    Simplifies routes

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,13 @@ use App\Http\Controllers\Task\ListCompleted\ListCompletedTaskController;
 use App\Http\Controllers\Task\View\ViewTaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/tasks', [CreateTaskController::class, 'create'])->name('tasks.create');
-Route::post('/tasks/{task}', [ChangeTaskController::class, 'change'])->name('tasks.change');
-Route::post('/tasks/{task}/delete', [DeleteTaskController::class, 'delete'])->name('tasks.delete');
-Route::post('/tasks/{task}/complete', [CompleteTaskController::class, 'complete'])->name('tasks.complete');
-Route::get('/tasks', [ListTaskController::class, 'list'])->name('tasks.list');
-Route::get('/tasks/completed', [ListCompletedTaskController::class, 'list_completed'])->name('tasks.list_completed');
-Route::get('/tasks/{task}', [ViewTaskController::class, 'view'])->name('tasks.view');
+Route::prefix('/tasks')->name('tasks.')->group(function () {
+    Route::post('/', [CreateTaskController::class, 'create'])->name('create');
+    Route::post('/{task}', [ChangeTaskController::class, 'change'])->name('change');
+    Route::post('/{task}/delete', [DeleteTaskController::class, 'delete'])->name('delete');
+    Route::post('/{task}/complete', [CompleteTaskController::class, 'complete'])->name('complete');
+
+    Route::get('/', [ListTaskController::class, 'list'])->name('list');
+    Route::get('/completed', [ListCompletedTaskController::class, 'list_completed'])->name('list_completed');
+    Route::get('/{task}', [ViewTaskController::class, 'view'])->name('view');
+});
